024383b808d0275eb689f22a9654b3d7fc42b8e9
JannikZed <JannikZed@users.noreply.github.com>
2015-02-06 08:49:15 +0100

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,10 @@
 name             'icinga2_db_create'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       ''
+maintainer_email 'zinkljannik@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures icinga2_db_create'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'icinga2'
+depends 'postgresql'
